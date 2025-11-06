d7e3ae088887fc31edbd92e7ace6c2ff233fa2b4
wjm <wjm9765.naver.com>
2025-11-06 23:15:34 +0900

    [FIX] CORS 수정

--- src/main/java/com/dealchain/dealchain/config/SecurityConfig.java
+++ src/main/java/com/dealchain/dealchain/config/SecurityConfig.java
@@ -32,7 +32,6 @@ public class SecurityConfig {
 
                     //나중에 삭제 필요
                     config.setAllowedOriginPatterns(List.of("http://localhost:*"));
-                    config.setAllowedOriginPatterns(List.of("http://127.0.0.1:*"));
 
 
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
